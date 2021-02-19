8960e0167f1b3f3d7078986f43d1a015543aef77
mohamedegila <mohammedegila@gmail.com>
2021-02-20 01:16:13 +0200

    updates in header, change profile page

--- server.js
+++ server.js
@@ -109,6 +109,7 @@ app.post("/login.html", bodyParser, auth.controler.login)
 app.post("/register.html", bodyParser, auth.controler.register)
 
 
+
 app.get("/products/productList.html", products.controler.allproductsView)
 app.get("/products/productList.html/:id", products.controler.productsViewByPage)
 app.post("/products/add.html", bodyParser, products.controler.add)
